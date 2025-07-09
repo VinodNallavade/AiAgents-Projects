a23f876fe0f5ff16cb066d4e297d01c65f225ac9
Vinod Nallavade <Vinod.Nallavade@gmail.com>
2025-07-09 15:38:22 +0530

    changed few things to make it work on streamlit cloud

--- RAG/app.py
+++ RAG/app.py
@@ -1,5 +1,9 @@
 from src.ui.main import load_app
 import asyncio
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 if __name__ == "__main__":
     asyncio.run(load_app())
